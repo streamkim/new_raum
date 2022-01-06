9ca26c1dfd5d74617d06cfb6ce7a475e7d13ed5e
streamkim <tlso1573@naver.com>
2022-01-06 10:04:21 +0900

    이미지변경

--- js/main.js
+++ js/main.js
@@ -9,27 +9,27 @@ $(function(){
     });
 
     // 사이드바
-    $('.btn-sidebar-open').click(function(){
-        $('.btn-sidebar-open').hide();
-        $('#sidebar').animate({right: 0},600);
-    });
-    $('.btn-sidebar-close').click(function(){
-        $('.btn-sidebar-open').show();
-        $('#sidebar').animate({right: -212},600);
-    });
+    if($(window).width() > 480) {
+        $('.btn-sidebar-open').click(function(){
+            $('.btn-sidebar-open').hide();
+            $('#sidebar').animate({right: 0},600);
+        });
+        $('.btn-sidebar-close').click(function(){
+            $('.btn-sidebar-open').show();
+            $('#sidebar').animate({right: -202},600);
+        });
+    };
 
     // 사이드바 모바일
     if($(window).width() <= 480) {
-
-    $('.btn-sidebar-open').click(function(){
-        $('.btn-sidebar-open').hide();
-        $('#sidebar').animate({right: 0},600);
-    });
-    $('.btn-sidebar-close').click(function(){
-        $('.btn-sidebar-open').show();
-        $('.m-sidebar').animate({right: -232},600);
-    });
-
+        $('.btn-sidebar-open').click(function(){
+            $('.btn-sidebar-open').hide();
+            $('#sidebar').animate({right: 0},600);
+        });
+        $('.btn-sidebar-close').click(function(){
+            $('.btn-sidebar-open').show();
+            $('#sidebar').animate({right: -222},600);
+        });
     };
 
     // a태그 상단 이동 막기
@@ -48,7 +48,7 @@ $(function(){
         var index = $this.index();
         
         $slider.find(' > .slides > .active').removeClass('active');
-        $slider.find(' > .slides > div').eq(index).addClass('active');
+        $slider.find(' > .slides > li').eq(index).addClass('active');
     });
     
     $('.slider > .side-btns > div').click(function() {
@@ -84,5 +84,45 @@ $(function(){
         $nextBtn.click();
     }
     
-    setInterval(Slider1__moveNext, 3000);
+    setInterval(Slider1__moveNext, 5000);
+
+    // 모바일 매거진 슬라이드
+    $('.mslider > .mpages > div').click(function() {
+        var $this = $(this);
+        var $slider = $this.closest('.mslider');
+        
+        $this.addClass('active');
+        $this.siblings('.active').removeClass('active');
+        
+        var index = $this.index();
+        
+        $slider.find(' > .mslides > .active').removeClass('active');
+        $slider.find(' > .mslides > li').eq(index).addClass('active');
+    });
+    
+    $('.mslider > .mside-btns > div').click(function() {
+        var $this = $(this);
+        var index = $this.index();
+        var $slider = $this.closest('.mslider');
+        
+        var $current = $slider.find('.mpages > div.active');
+        var $post;
+        
+        if ( index == 0 ) {
+            $post = $current.prev();
+        }
+        else {
+            $post = $current.next();
+        }
+        
+        if ( $post.length == 0 ) {
+            if ( index == 0 ) {
+                $post = $slider.find('.mpages > div:last-child');
+            }
+            else {
+                $post = $slider.find('.mpages > div:first-child');
+            }
+        }
+        $post.click();
+    });
 })
